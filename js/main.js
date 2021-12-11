0c46e4f072286ee4ef58cc90e6394223eec94aba
kitkes <nikitast68@gmail.com>
2021-12-11 16:24:15 +0300

    create: added map and booking block. added style.

--- js/main.js
+++ js/main.js
@@ -11,3 +11,19 @@ const swiper = new Swiper(".swiper", {
     prevEl: ".hotel-slider__button__prev",
   },
 });
+
+type = "text/javascript" > ymaps.ready(init);
+function init() {
+  var myMap = new ymaps.Map("map", {
+    center: [7.57413952, 79.80234031],
+    zoom: 10,
+  });
+  // Создание метки
+  var myPlacemark = new ymaps.Placemark(
+    // Координаты метки
+    [7.57413952, 79.80234031],
+  );
+
+  // Добавление метки на карту
+  myMap.geoObjects.add(myPlacemark);
+}
