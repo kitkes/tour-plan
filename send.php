fdce891c36dd1f028e3b2a396c9e8a615e0615e3
kitkes <nikitast68@gmail.com>
2021-12-28 06:23:49 +0300

    create php file. added phpmailer

---
+++ send.php
@@ -0,0 +1,58 @@
+<?php
+// Файлы phpmailer
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+// Переменные, которые отправляет пользователь
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+$email = $_POST['email'];
+
+// Формирование самого письма
+$title = "Новое обращение Best Tour Plan";
+$body = "
+<h2>Новое обращение от:</h2>
+<b>Имя:</b> $name<br>
+<b>Телефон:</b> $phone<br><br>
+<b>Сообщение:</b><br>$message
+";
+
+// Настройки PHPMailer
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+try {
+    $mail->isSMTP();   
+    $mail->CharSet = "UTF-8";
+    $mail->SMTPAuth   = true;
+    $mail->SMTPDebug = 2;
+    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+
+    // Настройки вашей почты
+    $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
+    $mail->Username   = 'nikitast68@gmail.com'; // Логин на почте
+    $mail->Password   = 'mvvzbwsomdoabpfl'; // Пароль на почте
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
+    $mail->setFrom('nikitast68@stukovnik.ru', 'Никита Стуков'); // Адрес самой почты и имя отправителя
+    $mail->FromName = 'stukovnik.ru';
+
+    // Получатель письма
+    $mail->addAddress('nikitosik999.98@mail.ru');
+
+    // Отправка сообщения
+    $mail->isHTML(true);
+    $mail->Subject = $title;
+    $mail->Body = $body;    
+
+// Проверяем отравленность сообщения
+if ($mail->send()) {$result = "success";} 
+else {$result = "error";}
+
+} catch (Exception $e) {
+    $result = "error";
+    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
+
+// Отображение результата
+header('Location: thankyou.html');
